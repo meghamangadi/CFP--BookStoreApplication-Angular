3f59b775a5dc215cc3180cf1e6797282946d9e19
megha angadi <meghamangadi@gmail.com>
2021-12-30 08:40:53 +0530

    [Megha] Modified : Code

--- src/app/common/all-books/all-books.component.ts
+++ src/app/common/all-books/all-books.component.ts
@@ -1,10 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import {  BookModel } from 'src/app/Model/book-model';
-import { Router } from "@angular/router"; 
+import { BookModel } from 'src/app/Model/book-model';
+import { Router } from "@angular/router";
 import { MatSnackBar } from '@angular/material/snack-bar';
-import {  BookServiceService } from 'src/app/service/book-service.service';
-import {CartModel} from 'src/app/Model/CartModel ';
-import{CartServiceService} from 'src/app/service/cart-service.service'
+import { BookServiceService } from 'src/app/service/book-service.service';
+import { CartModel } from 'src/app/Model/CartModel ';
+import { CartServiceService } from 'src/app/service/cart-service.service'
 @Component({
   selector: 'app-all-books',
   templateUrl: './all-books.component.html',
@@ -17,64 +17,60 @@ export class AllBooksComponent implements OnInit {
 
   hideButton: boolean = false;
 
-  userId : any = localStorage.getItem('userId') ;
+  userId: any = localStorage.getItem('userId');
 
-  body : any;
-   
- itemExists : number [] = [];
+  body: any;
+
+  itemExists: number[] = [];
   public cartModel: CartModel = new CartModel;
-  
-  constructor(private bookServiceService: BookServiceService,private cartServiceService:CartServiceService, private route: Router,   private matSnackBar: MatSnackBar) { }
+
+  constructor(private bookServiceService: BookServiceService, private cartServiceService: CartServiceService, private route: Router, private matSnackBar: MatSnackBar) { }
 
   ngOnInit(): void {
     this.BookDetails();
-       
+
     this.cartServiceService.search.subscribe(val => {
-      
+
       this.searchTerm = val
     });
   }
 
   BookDetails() {
-     this.bookServiceService.getApiCall().subscribe(data => {
-       this.bookDetails = data.object;
-     })
+    this.bookServiceService.getApiCall().subscribe(data => {
+      this.bookDetails = data.object;
+    })
   }
-  addToCart(bookId: number ){
-   this.cartModel.bookId=bookId;
-   this.cartModel.quantity=1;
-   this.hideButton = true;
-    console.log("cart deatils" +bookId  );
-    
-  //  this.cartServiceService.addToCart(this.cartModel).subscribe((response: any)=>{this.BookDetails();});
-    ///
- 
+  addToCart(bookId: number) {
+    this.cartModel.bookId = bookId;
+    this.cartModel.quantity = 1;
+    this.hideButton = true;
+    console.log("cart deatils" + bookId);
 
-  this.cartServiceService.addToCart(this.cartModel).subscribe(response  => {
-    
-    if( bookId && !this.itemExists.includes(bookId)){
-
-      console.log("Helloooooo",bookId)
-      this.itemExists.push(bookId);
-    }
-  });
-  this.matSnackBar.open('Book added to your Cart successfully...!!' , 'ok', {
-    duration: 5000
-  });
-  this.route.navigate(['header'])
-   this.route.navigate(['dashboard'])
+    //  this.cartServiceService.addToCart(this.cartModel).subscribe((response: any)=>{this.BookDetails();});
     ///
+
+
+    this.cartServiceService.addToCart(this.cartModel).subscribe(response => {
+
+      if (bookId && !this.itemExists.includes(bookId)) {
+
+        console.log("Helloooooo", bookId)
+        this.itemExists.push(bookId);
+      }
+    });
+    this.matSnackBar.open('Book added to your Cart successfully...!!', 'ok', {
+      duration: 5000
+    });
+    this.route.navigate(['header'])
+    this.route.navigate(['dashboard'])
+
   }
   sortByPriceLowToHigh() {
-    // this.bookService.sortByPriceLowToHigh().subscribe(data => {
-    //   this.bookDetails = data
-    // });
+
   }
 
   sortByPriceHighToLow() {
-    // this.bookService.sortByPriceHighToLow().subscribe(data => {
-    //   this.bookDetails = data;
-    // })
+
   }
 
   /**
@@ -87,9 +83,6 @@ export class AllBooksComponent implements OnInit {
   }
 
   searchBook() {
-    // this.bookService.searchBookByUserInput(this.bookService.searchTerm).subscribe(data => {
-    //   console.log(data)
-    // })
 
   }
 }
